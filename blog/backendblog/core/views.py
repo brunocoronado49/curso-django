2370b5064eb8e05662c517ef8ba2d6a5e12928d0
Francisco Rangel <francorangelcoronado@gmail.com>
2024-02-14 22:55:16 -0600

    Creando los templates para las vistas

--- blog/backendblog/core/views.py
+++ blog/backendblog/core/views.py
@@ -1,24 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
-html_base = '''
-<h1>My personal web</h1>
-<hr>
-
-<ul>
-    <li>
-        <a href="/">Home</a>
-    </li>
-    <li>
-        <a href="/about">About me</a>
-    </li>
-</ul>
-'''
 
 # Create your views here.
 def home(request):
-    html_response = f'{html_base} <h2>Home</h2>'
-    return HttpResponse(html_response)
+    return render(request, 'core/home.html')
 
 def about(request):
-    html_response = f'{html_base} <h2>About me</h2>'
-    return HttpResponse(html_response)
+    return render(request, 'core/about.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
